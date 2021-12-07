20a4f4cc967cc02098c8f017a2b3d6bc91cfa210
Rob Bos <raj.bos@gmail.com>
2021-12-07 16:49:14 +0100

    Added workflow to update dev.to posts

---
+++ .github/workflows/dev-to-script.js
@@ -0,0 +1,15 @@
+module.exports = async ({devtoToken, axios}) => {
+  console.log(`devtoToken: ${devtoToken}`);
+
+  const instance = axios.create({
+    baseURL: 'https://dev.to/api',
+    timeout: 1000,
+    headers: {'api-key': `$devtoToken`},
+  });
+
+  instance.get('/articles/me/unpublished')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+}
